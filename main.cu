74945402535cdfe32a4616de63530fc56dc2ba6e
Carson Fox <carson.fox@atomicjolt.com>
2020-11-22 15:47:22 -0500

    Void kernel runs, rotation on cpu

--- main.cu
+++ main.cu
@@ -2,9 +2,20 @@
 
 #include "Image.hpp"
 
+__global__ void kernel() {}
+
 int main() {
-    Image inputImage;
-    std::cin >> inputImage;
-    std::cout << inputImage;
+    auto inputImage = new Image(), outputImage = new Image();
+
+    std::cin >> *inputImage;
+
+    for (int i = 0; i < IMAGE_SIZE; i++) {
+        for (int j = 0; j < IMAGE_SIZE; j++) {
+            outputImage->at(i).at(j) = inputImage->at(j).at(i);
+        }
+    }
+
+    std::cout << *outputImage
+
     return 0;
 }
