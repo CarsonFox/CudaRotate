0aefa912577744863e6ba889a76e87f2875f0184
Carson Fox <carson.fox@atomicjolt.com>
2020-11-22 17:16:37 -0500

    Cuda hello and target image

--- main.cu
+++ main.cu
@@ -2,7 +2,31 @@
 
 #include "Image.hpp"
 
-__global__ void kernel() {}
+__global__ void add(int *a, int *b, int *out) {
+    *out = *a + *b;
+}
+
+void cudaHello() {
+    int a = 0, b = 0, out = 0;
+    int *d_a, *d_b, *d_out;
+
+    cudaMalloc(&d_a, sizeof(int));
+    cudaMalloc(&d_b, sizeof(int));
+    cudaMalloc(&d_out, sizeof(int));
+
+    a = 5, b = 11;
+
+    cudaMemcpy(d_a, &a, sizeof(int), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_b, &b, sizeof(int), cudaMemcpyHostToDevice);
+
+    add<<<1,1>>>(d_a, d_b, d_out);
+
+    cudaMemcpy(&out, d_out, sizeof(int), cudaMemcpyDeviceToHost);
+
+    cudaFree(d_a), cudaFree(d_b), cudaFree(d_out);
+
+    std::cout << a << " + " << b << " = " << out << std::endl;
+}
 
 int main() {
     auto inputImage = new Image(), outputImage = new Image();
