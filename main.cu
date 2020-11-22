fb46d9a4dceee07c0c44c44d74ed70a04225aaf4
Carson Fox <carson.fox@atomicjolt.com>
2020-11-22 17:23:05 -0500

    call hello kernel

--- main.cu
+++ main.cu
@@ -29,17 +29,7 @@ void cudaHello() {
 }
 
 int main() {
-    auto inputImage = new Image(), outputImage = new Image();
-
-    std::cin >> *inputImage;
-
-    for (int i = 0; i < IMAGE_SIZE; i++) {
-        for (int j = 0; j < IMAGE_SIZE; j++) {
-            (*outputImage)[i][j] = (*inputImage)[j][i];
-        }
-    }
-
-    std::cout << *outputImage;
+    cudaHello();
 
     return 0;
 }
