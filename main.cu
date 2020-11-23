7cc4e725bfa81fab50255cd777fa041b1d64097f
Carson Fox <carson.fox@atomicjolt.com>
2020-11-22 21:08:24 -0500

    skeleton

--- main.cu
+++ main.cu
@@ -2,34 +2,23 @@
 
 #include "Image.hpp"
 
-__global__ void add(int *a, int *b, int *out) {
-    *out = *a + *b;
-}
-
-void cudaHello() {
-    int a = 0, b = 0, out = 0;
-    int *d_a, *d_b, *d_out;
-
-    cudaMalloc(&d_a, sizeof(int));
-    cudaMalloc(&d_b, sizeof(int));
-    cudaMalloc(&d_out, sizeof(int));
-
-    a = 5, b = 11;
-
-    cudaMemcpy(d_a, &a, sizeof(int), cudaMemcpyHostToDevice);
-    cudaMemcpy(d_b, &b, sizeof(int), cudaMemcpyHostToDevice);
-
-    add<<<1,1>>>(d_a, d_b, d_out);
-
-    cudaMemcpy(&out, d_out, sizeof(int), cudaMemcpyDeviceToHost);
-
-    cudaFree(d_a), cudaFree(d_b), cudaFree(d_out);
-
-    std::cout << a << " + " << b << " = " << out << std::endl;
-}
-
 int main() {
-    cudaHello();
+    auto inputImage = new Image(), outputImage = new Image();
+    Image *d_image;
+    cudaMalloc(&d_image, sizeof(Image));
 
+    std::cin >> *inputImage;
+
+    cudaMemcpy(d_image, inputImage, sizeof(Image), cudaMemcpyHostToDevice);
+
+    //Kernel
+
+    cudaMemcpy(outputImage, d_image, sizeof(Image), cudaMemcpyDeviceToHost);
+
+    std::cout << *outputImage;
+
+    delete inputImage;
+    delete outputImage;
+    cudaFree(d_image);
     return 0;
 }
